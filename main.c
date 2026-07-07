3bc139bde572bd786450c127cc8e737a5fd79e54
M-Ali007 <mustafa.murta08@gmail.com>
2026-07-06 20:51:43 -0600

    added arg logic to process flags, changed plan to add more flags

--- main.c
+++ main.c
@@ -4,11 +4,46 @@
 // -f -> to use commands with flashdecks 
 // -n -> to use commands with notes
 // -b -> browse all notes / flashcard decks
-// -o <note/flashdeck name> -> open note / flashdeck 
+// -r <note/flashdeck name> -> open note / flashdeck 
 // -e <note/flashdeck name> -> edit note / flashdeck
-// -c <note/flashdeck name> -> create note / flashdeck
+// -a <note/flashdeck name> -> create note / flashdeck
+// -d <note/flashdeck name> -> delete note / flashdeck
+// -h -> display help
+
 
 int main(int argc, char *argv[]) {
-
+	if (argc < 2){
+		printf("Please enter valid flag, use -h flag for man pages.");
+	}
+	for (int i = 1; i < argc; i++){
+		if (argv[i][0] == '-') {
+			switch(argv[i][1]) {
+				case 'f':
+					printf("Flashcards Mode");
+					break;
+				case 'n':
+					printf("Notes Mode");
+					break;
+				case 'b':
+					printf("Browse");
+					break;
+				case 'r':
+					printf("Read");
+					break;
+				case 'e':
+					printf("Edit");
+					break;
+				case 'a':
+					printf("Create");
+					break;
+				case 'd':
+					printf("Delete");
+					break;
+				case 'h':
+					printf("Help");
+					break;
+			}
+		}
+	}
 	return 0;
 }
