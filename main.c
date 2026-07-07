935802832da2d637f8324d26ca5a1696b161b457
M-Ali007 <mustafa.murta08@gmail.com>
2026-07-06 19:10:24 -0600

    intial commit: added basic code and created basic plan in comments

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// flags - 
+// -f -> to use commands with flashdecks 
+// -n -> to use commands with notes
+// -b -> browse all notes / flashcard decks
+// -o <note/flashdeck name> -> open note / flashdeck 
+// -e <note/flashdeck name> -> edit note / flashdeck
+// -c <note/flashdeck name> -> create note / flashdeck
+
+int main(int argc, char *argv[]) {
+
+	return 0;
+}
